b34e951132b477dcd5b87d5382c76374469cc1d2
Zhansaya <zhansaya@umail.ucsb.edu>
2015-06-01 22:59:06 -0700

    set default location

--- CS185Final/app/src/main/java/edu/ucsb/cs/cs185/cs185final/MapsActivity.java
+++ CS185Final/app/src/main/java/edu/ucsb/cs/cs185/cs185final/MapsActivity.java
@@ -68,7 +68,12 @@ public class MapsActivity extends FragmentActivity {
         Criteria criteria = new Criteria();
         String locationProvider = locationManager.getBestProvider(criteria, false);
         Location location = locationManager.getLastKnownLocation(locationProvider);
-        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+        LatLng latlng;
+        if(location != null){
+            latlng = new LatLng(location.getLatitude(), location.getLongitude());
+        }
+        else latlng = new LatLng(0,0);
+
         mMap.setMyLocationEnabled(true);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 13));
         mMap.addMarker(new MarkerOptions()
